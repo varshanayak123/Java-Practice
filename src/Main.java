3523389a036dc84f0dd0c7118bc2959780ef0102
varshanayak123 <vn8646964@gmail.com>
2026-08-16 21:34:40 +0530

    Added

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
 import java.util.Scanner;
-   public class Main{
-public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.println("Enter a Number: ");
-    float num = input.nextFloat();
-    if(num % 2 == 0){
-        System.out.println(num + " is prime number");
-    }else {
-        System.out.println(num + " is odd number");
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int rev = 0;
+        int count = 0;
+            while(n > 0) {
+                int rem = n % 10;
+                rev = rev*10+rem;
+                n = n/10;
+                count++;
+            }
+        System.out.println(rev);
+
+        }
+
     }
-}
-}
